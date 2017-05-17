60a560c92638bb5314b334e40191e76008024450
yanessa <yanessaa@yahoo.com>
2017-05-16 23:40:33 -0400

    week 2: case studies, acf, featured work and work landing page

---
+++ wp-content/themes/accelerate-theme-child/archive-case_studies.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * The template for displaying archive page
+ *
+ *
+ * @package WordPress
+ * @subpackage Accelerate Marketing
+ * @since Accelerate Marketing 1.0
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="site-content">
+		<div id="content" role="main">
+			<?php while ( have_posts() ) : the_post();
+        $size = "full";
+        $services = get_field ('services');
+        $image_1 = get_field ('image_1');
+      ?>
+
+      <article class="case-study">
+        <aside class="case-study-sidebar">
+          <h2><a href="<?php the_permalink;?>"><?php the_title(); ?></a></h2>
+          <h5><?php echo $services; ?></h5>
+
+          <?php the_excerpt(); ?>
+
+          <p><strong><a href="<?php the_permalink;?>">View Project</a></strong></p>
+
+        </aside> <!-- case study sidebar -->
+
+        <div class="case-study-images">
+          <a href="<?php the_permalink;?>">
+            <?php if ($image_1) { ?>
+              <?php echo wp_get_attachment_image( $image_1, $size ); ?>
+            <?php } ?>
+          </a>
+
+        </div> <!-- case study sidebar images -->
+
+      </article>
+
+			<?php endwhile; // end of the loop. ?>
+
+		</div><!-- #content -->
+	</div><!-- #primary -->
+
+<?php get_footer(); ?>
